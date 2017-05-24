1ccc810d0c5efd3a735636236440a14a6a3b1590
Alexander Kolbin <kolbin_aa@yahoo.com>
2017-05-24 17:17:05 -0400

    feat: Create class MapViewController
    Set up delegate to class

---
+++ ShoreApp/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  ShoreApp
+//
+//  Created by mac on 5/24/17.
+//  Copyright © 2017 Alder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+}
